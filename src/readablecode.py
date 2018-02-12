fae3c62a3c81b165b7dab1a5437c4b32e163fe14
황석진 <dollking@users.noreply.github.com>
2018-02-13 00:43:46 +0900

    start project & make a basic structure.

--- src/readablecode.py
+++ src/readablecode.py
@@ -2,4 +2,5 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-P', '--path', help='Check readability of the code in directory.', action='store', default=None)
+    parser.add_argument('-T', '--text', help='Output the result to text file.', action='store', default=None)
+    parser.add_argument('-E', '--excel', help='Output the result to excel file.', action='store', default=None)
---
+++ test/testData/test2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int temp1, temp2, pos;
+    std::cin >> temp1 >> temp2 >> pos;
+
+    if(temp1 == pos)
+        printf("OK\n");
+
+    return 0;
+}
